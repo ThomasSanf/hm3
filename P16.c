c6878691f0fc72629ecfc073c221cf21827c91a7
Thomas Sanfourche <58660377+ThomasSanf@users.noreply.github.com>
2023-11-07 14:03:16 +0800

    Update P16.c

--- P16.c
+++ P16.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int maximum(int x, int y, int z);
+int main(void)
+{
+    int x;
 
-int main(void) {
-    int number1;
-    int number2;
-    int number3;
-    printf("Enter three integers: ");
-    scanf("%d %d %d", &number1, &number2, &number3);
-    printf("Maximum is: %d\n", maximum(number1, number2, number3));
+    for(x = 1; x <= 10; x++)
+    {
+        printf("%.2f ", sqrt(x));
+    }
+    printf("\n");
     system("pause");
     return 0;
-}
-
-int maximum(int x, int y, int z) {
-    int max = x;
-    if (y > max) {
-        max = y;
-    }
-    if (z > max) {
-        max = z;
-    }
-    return max;
 }
